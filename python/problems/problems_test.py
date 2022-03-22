50583489b2bc24948d0cbc82b8e8ef25694c6cd5
Troy Schwab <trschwab7@gmail.com>
2022-03-21 20:17:50 -0400

    isolate imports to another file for testing..

--- python/problems/problems_test.py
+++ python/problems/problems_test.py
@@ -1,32 +1,5 @@
 import pytest
-from fibonacci import *
-from steps import *
-from min_cost_stairs import *
-from palindrome_integers import *
-from roman_to_integer import *
-from two_sum import two_sum
-from add_two_numbers import *
-from two_sum_IV import *
-from range_sum_of_bst import *
-from fizz_buzz import *
-from valid_parentheses import *
-from longest_common_prefix import *
-from longest_substring_without_repeating_characters import *
-from merge_two_sorted_lists import *
-from implement_strstr import *
-from merge_k_sorted_lists import *
-from sqrtx import *
-from asteroid_collision import *
-from destroying_asteroids import *
-from climbing_stairs import *
-from valid_palindrome import *
-from intersection_of_two_linked_lists import *
-from remove_duplicates_from_sorted_array import *
-from first_unique_character_in_a_string import *
-from single_number import *
-from valid_anagram import *
-from number_of_1_bits import *
-from reverse_string import *
+from problems_test_imports import *
 
 def test_reverseString():
 	''' test reverseString '''
